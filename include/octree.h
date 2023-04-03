d13dc33bab4f6c7ed376c171abe9ca8ebaecb09f
NilEis <52820950+NilEis@users.noreply.github.com>
2023-04-03 22:17:43 +0200

    Müder 😴

--- include/octree.h
+++ include/octree.h
@@ -1,18 +1,6 @@
 #ifndef OCTREE_H
 #define OCTREE_H
 
-#include "particle.h"
 
-#define MAX_PARTICLES_PER_OCTANT 50
-
-typedef struct Octant Octant;
-
-Octant *new_octant(double center[3], double size);
-
-void insert_particle(Octant *octant, Particle *particle);
-
-void traverse_octree(Octant *octant, void (*func)(Particle *));
-
-int particle_in_octant(Particle *particle, Octant *octant);
 
 #endif // OCTREE_H
